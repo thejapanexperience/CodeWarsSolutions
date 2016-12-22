86e50786d5bb71986a25fe87e5726a6e7689266c
Richard Mands <richard@eduku.org>
2016-12-22 18:32:01 +0900

    update

--- 5KyuTwiceLinear/main.js
+++ 5KyuTwiceLinear/main.js
@@ -2,15 +2,19 @@ function dblLinear(n) {
   const u = [1];
 
   for (let i = 0; i < n; i++) {
+    const lastHighest = u[u.length - 1];
     const x = u[i];
+    const y = (2 * x) + 1;
+    if (u.indexOf(y) === -1) {
+      u.push(y);
+    }
+    if (y < lastHighest) {
+      u.sort((a, b) => a - b);
+    }
     const z = (3 * x) + 1;
     if (u.indexOf(z) === -1) {
       u.push(z);
     }
-    const y = (2 * x) + 1;
-    if (u.indexOf(y) === -1) {
-      u.push(y);
-    } u.sort((a, b) => a - b);
   }
   console.log('u[n]: ', u[n]);
   return (u[n]);
